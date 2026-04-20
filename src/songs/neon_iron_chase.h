4f1f09f849525e2ecf4b3f8127271ad8abf8762d
Julian Lastra <julianlastra@yahoo.com>
2026-04-20 10:39:21 -0300

    Added new song

---
+++ src/songs/neon_iron_chase.h
@@ -0,0 +1,8 @@
+#ifndef NEON_IRON_CHASE_H
+#define NEON_IRON_CHASE_H
+
+#include "song.h"
+
+extern const SongChart songNeonIronChase;
+
+#endif
--- src/songs/song_registry.c
+++ src/songs/song_registry.c
@@ -1,11 +1,13 @@
 #include "song_registry.h"
 #include "smoke_on_the_water.h"
 #include "seven_nation_army.h"
+#include "neon_iron_chase.h"
 
 const SongChart* const songList[] =
 {
     &songSmokeOnTheWater,
-    &songSevenNationArmy
+    &songSevenNationArmy,
+    &songNeonIronChase
 };
 
 const u16 songCount = sizeof(songList) / sizeof(songList[0]);
